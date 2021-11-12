adf52d119e7edd037667df5bd7ac528ab8675026
Johannes Weber <johannes@drweber.de>
2021-11-12 13:10:20 -0800

    Refactor breed fetching fn to take callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,27 @@
 const request = require('request');
 const SEARCH_BREEDS_URL = 'https://api.thecatapi.com/v1/breeds/search';
 
+const fetchBreedDescription = (query, endpoint, callback) => {
+  request(endpoint + '?q=' + query, (err, res, body) => {
+    if (!err) {
+      const data = JSON.parse(body); //Array expected
+      if (data.length > 0) {
+        //Return description for first breed returned from the API
+        callback(err, data[0].description);
+      } else {
+        //No error but no data either (no query or no results)
+        callback(err, data);
+      }
+    } else {
+      //Error - request failed
+      callback(err, body);
+    }
+  });
+};
+
+module.exports = fetchBreedDescription;
+
+/*
 const searchBreeds = (query, endpoint) => {
   request(endpoint + '?q=' + query, (err, res, body) => {
     if (!err) {
@@ -25,3 +46,4 @@ const searchBreeds = (query, endpoint) => {
 };
 
 searchBreeds(process.argv[2], SEARCH_BREEDS_URL);
+*/

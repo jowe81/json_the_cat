b6ea2f96a171e02f88aa0bc5e33e57331060b98b
Johannes Weber <johannes@drweber.de>
2021-11-12 12:36:15 -0800

    log out description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,15 @@
+const { type } = require('os');
 const request = require('request');
 const SEARCH_BREEDS_URL = 'https://api.thecatapi.com/v1/breeds/search';
 
 const searchBreeds = (query, endpoint) => {
   request(endpoint + '?q=' + query, (err, res, body) => {
-    console.log(body);
+    if (!err) {
+      const data = JSON.parse(body); //Array expected
+      for (let breed of data) {
+        console.log(breed.description);
+      }
+    }
   });
 };
 
